4b69a81832fe86fdbd9f0887cdc6ed198ac48df3
josh <jsnyder17@ycp.edu>
2023-10-26 11:32:00 -0400

    .gitignore is now working

--- kernel/arch/i386/gdt.d
+++
@@ -1,6 +0,0 @@
-arch/i386/gdt.o: arch/i386/gdt.c \
- /home/josh/Downloads/SnyderOS/sysroot/usr/include/arch/i386/gdt.h \
- /home/josh/opt/cross/lib/gcc/i686-elf/12.2.0/include/stdint.h \
- /home/josh/opt/cross/lib/gcc/i686-elf/12.2.0/include/stdint-gcc.h \
- /home/josh/Downloads/SnyderOS/sysroot/usr/include/stdio.h \
- /home/josh/Downloads/SnyderOS/sysroot/usr/include/sys/cdefs.h
